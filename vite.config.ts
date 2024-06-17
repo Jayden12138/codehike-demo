c6ee7a4c03dfc054397c7d9638c6751fb5d699d1
Jayden <18993882884@163.com>
2024-06-17 11:13:05 +0800

    feat: add codehike

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {remarkCodeHike} from '@code-hike/mdx'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+export default defineConfig(async () => {
+  const mdx = await import("@mdx-js/rollup")
+  return {
+    optimizeDeps: {
+      include: ["react/jsx-runtime"],
+    },
+    plugins: [
+      react(),
+      mdx.default({
+        remarkPlugins: [[remarkCodeHike, { theme: "material-palenight" }]],
+      }),
+    ],
+  }
 })
